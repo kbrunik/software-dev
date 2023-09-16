89fe30e5c51968516830ae44b6ea88fcc459bc02
Cameron Irmas <cirmas@protonmail.com>
2023-09-15 17:09:40 -0700

    Update for linter

--- tests/test_add.py
+++ tests/test_add.py
@@ -1,4 +1,5 @@
 from research_software_dev.guides.testing.add import add
 
+
 def test_add():
     assert add(1, 2) == 3
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,18 +1,22 @@
 import pytest
 from research_software_dev.guides.testing.calculator import Calculator
 
+
 # Fixture to set up the Calculator instance before tests
 @pytest.fixture
 def calculator():
     return Calculator()
 
+
 def test_addition(calculator):
     result = calculator.add(1, 2)
     assert result == 3, f"Expected 3 but got {result}"
 
+
 def test_subtraction(calculator):
     result = calculator.subtract(5, 3)
     assert result == 2, f"Expected 2 but got {result}"
+
 
 def test_multiplication(calculator):
     result = calculator.multiply(3, 4)
